50a997b03f005ebbf1d65ea8263164581eb94ec4
zidanreksa <zidanreksa789@gmail.com>
2023-10-16 08:31:45 +0700

    Github pemrograman komputer 1 project

---
+++ src/javaapplication1/Koneksi.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author LENOVO
+ */
+public class Koneksi {
+    private static Connection koneksi;
+    
+    public static Connection getConnection() throws SQLException {
+        String db="jdbc:mysql://localhost:3306/dbcrew";
+        String user = "root";
+        String pass = "";
+        
+        if(koneksi==null) {
+            koneksi = DriverManager.getConnection(db,user,pass);
+        }
+        return koneksi;
+    }
+}
--- src/javaapplication1/Project.java
+++ src/javaapplication1/Project.java
@@ -29,17 +29,20 @@ public class Project extends javax.swing.JFrame {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbcrew", "root", "");
+            Tampil(Sql);  
         } catch (Exception e) {
             System.out.println("Koneksi Gagal" + e.getMessage());
         }
     }
     private void Tampil(String Sql) {
         DefaultTableModel kolomkolom = new DefaultTableModel();
-        kolomkolom.addColumn("ID Absensi QR");
+        kolomkolom.addColumn("ID Karyawan");
         kolomkolom.addColumn("Nama Karyawan");
-        kolomkolom.addColumn("Tanggal dan Waktu Absensi");
+        kolomkolom.addColumn("Posisi");
+        kolomkolom.addColumn("Jenis Kelamin");
+        kolomkolom.addColumn("Tanggal Absensi dan Waktu Absensi");
         kolomkolom.addColumn("Keterangan");
-        kolomkolom.addColumn("Data QR");
+        kolomkolom.addColumn("Nomor HP");
 
         try {
             st = Con.createStatement();
@@ -47,7 +50,7 @@ public class Project extends javax.swing.JFrame {
             while (Rs.next()) {
                 kolomkolom.addRow(new Object[] {
                     Rs.getString(1), Rs.getString(2), Rs.getString(3),
-                    Rs.getString(4), Rs.getString(5)});
+                    Rs.getString(4), Rs.getString(5),Rs.getString(6),Rs.getString(7)});
             }
             Tabeldata.setModel(kolomkolom);
         } catch (Exception e) {
@@ -72,25 +75,26 @@ public class Project extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("DATA ABSENSI KARYAWAN");
 
         Tabeldata.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
             },
             new String [] {
-                "ID Absensi QR", "Nama Karyawan", "Tanggal dan Waktu Absensi", "Keterangan", "Data QR"
+                "ID Karyawan", "Nama Karyawan", "Posisi", "Jenis Kelamin", "Tanggal Absensi dan Waktu Absensi", "Keterangan", "Nomor HP"
             }
         ));
         jScrollPane1.setViewportView(Tabeldata);
 
         jButton1.setBackground(new java.awt.Color(102, 255, 255));
-        jButton1.setText("Tampilkan");
+        jButton1.setText("Tambah Anggota");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -102,13 +106,11 @@ public class Project extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 798, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(786, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(36, 36, 36))
+            .addComponent(jScrollPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -125,7 +127,7 @@ public class Project extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       Tampil(Sql);   // TODO add your handling code here:
+       new TambahKaryawan().setVisible(true);  // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
